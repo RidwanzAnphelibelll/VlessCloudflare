06e9e9fbe02f81cb5c1e223b062c349e04c86eee
@RS <152822890+RidwanzAnphelibelll@users.noreply.github.com>
2025-06-22 07:56:54 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 
   userID: 'e85d97bb-7371-4603-9e29-db25848e93c6',
   proxyIPs: ['68.183.227.45'], //sgdo
-  dohURL: 'https://cloudflare-dns.com/dns-query',
+  dohURL: 'https://freedns.controld.com/p0',
   
   cnHostnames: [
     'cf.shopee.co.id'
